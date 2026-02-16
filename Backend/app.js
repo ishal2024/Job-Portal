45bebf0f4592f7e8424850a53bbdba5ac46ba0f4
vishaljha-adm <vishal.jha@posistrength.in>
2026-02-16 18:33:41 +0530

    CORS Change

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,10 @@ app.use(express.urlencoded({extended : true}))
 app.use(parser())
 
 app.use(cors({
-    origin : "https://job-hunt-test.vercel.app",
+    origin : [
+        "https://job-hunt-test.vercel.app",
+        "http://localhost:5173",
+    ],
     credentials : true,
     // methods: ["GET", "POST", "PUT", "DELETE"],
     // allowedHeaders: ["Content-Type", "Authorization"]
